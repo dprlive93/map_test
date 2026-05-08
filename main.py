bd422792869f3c5c99328af828243c9e16777da8
dprlive93 <kko77-s250106@gclass.ice.go.kr>
2026-05-08 15:51:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import folium
 from streamlit_folium import st_folium
 import pandas as pd
 
-#1. 웹 페이지 설정
+# 1. 웹 페이지 설정
 st.set_page_config(page_title="남동고 등산 메이트",layout="Wide")
-
+st.title("2026 학교 등산 행사 안내 지도")
+st.markdown("우리 동아리가 직접 발로 뛰며 만든 코스 가이드 입니다.")
+st.markdown("응")
 st.text("hedddddddddddddllo")
 
 
