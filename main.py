5abf65b4564ab13a04acf255163b614ab84c841f
dprlive93 <kko77-s250106@gclass.ice.go.kr>
2026-05-08 16:01:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 # 1. 웹 페이지 설정
 st.set_page_config(page_title="남동고 등산 메이트",layout="Wide")
+
 st.title("2026 학교 등산 행사 안내 지도")
 st.markdown("우리 동아리가 직접 발로 뛰며 만든 코스 가이드 입니다.")
 st.markdown("왼쪽 메뉴에서 코스를 선택하고 행사에 참여해 보세요.")
@@ -13,4 +14,7 @@ st.text("#큰 제목")
 st.text("## 작은 제목")
 st.text(" **굵은 글씨** 와 *이탤릭체* ")
 
-
+st.header("헤더입니다")
+st.subheader("서브헤더입니다")
+st.caption("캡션입니다")
+st.code("a=3")
