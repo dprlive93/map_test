cd8d2a0e236193dea6a0c6173e9de6cdaf4559dc
dprlive93 <kko77-s250106@gclass.ice.go.kr>
2026-08-21 15:29:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ else:
     filtered_df = df[df['코스'] == selected_course].copy()
 
 m = folium.Map(
-    location = [37.40583317, 126.7214872],
+    location = [37.407514, 126.7214872],
     zoom_start= 16
 )
 #for i in range(len(df)):
